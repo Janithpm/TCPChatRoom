4782165628105aef9d2043fa45090dd6d55bc2e2
Janith Madarasinghe <janithpm9991@gmail.com>
2021-09-07 22:25:21 +0530

    IP

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import threading
 import socket
 
+HOST = '127.0.0.1'
+PORT = 55555
+
 nickname = input("Enter a nickname : ")
 
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect((HOST,PORT ))
 
 def receive():
     while True:
